acfe61be0945fb5e2f928f2f823b99fa202a91f2
王振 <1260323835@qq.com@example.com>
2021-10-27 09:16:11 +0800

    feat(components): 新增错误页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@
  * @Author: 王振
  * @Date: 2021-09-23 15:14:02
  * @LastEditors: 王振
- * @LastEditTime: 2021-10-26 15:03:46
+ * @LastEditTime: 2021-10-27 09:13:58
  */
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
@@ -23,6 +23,14 @@ const routes: Array<RouteRecordRaw> = [
       title: '登录',
     },
     component: () => import(/* webpackChunkName: "about" */ '../views/Login/Login.vue'),
+  },
+  {
+    path: '/404',
+    name: 'ErrorPage',
+    meta: {
+      title: '登录',
+    },
+    component: () => import(/* webpackChunkName: "about" */ '../views/ErrorPage/ErrorPage.vue'),
   },
 ];
 
